6025e9e7f0a7067702fc74a578c08935541636c1
yu840915 <yu840915@gmail.com>
2025-03-02 00:09:33 +0800

    Make error message public

--- Sources/RemoteCameraCore/MessageCodingPort/ErrorMessage.swift
+++ Sources/RemoteCameraCore/MessageCodingPort/ErrorMessage.swift
@@ -1,9 +1,24 @@
-struct ErrorMessage: Error {
-  let domain: String
-  let code: Int
-  let message: String?
+public struct ErrorMessage: Error, Equatable {
+  public let domain: String
+  public let code: Int
+  public let message: String?
+
+  public init(domain: String, code: Int, message: String? = nil) {
+    self.domain = domain
+    self.code = code
+    self.message = message
+  }
 }
 
 protocol ErrorMessageConvertible: Error {
   var toErrorMessage: ErrorMessage { get }
+}
+
+public struct ErrorMessageCoders {
+  public typealias ErrorEncoder<Data> = MessageEncodingServicePort<
+    ErrorMessage, Data
+  >
+  public typealias ErrorDecoder<Data> = MessageDecodingServicePort<
+    Data, ErrorMessage
+  >
 }
