61ab963b4b26f9c2bb851ca2f158e2d06a8cbf95
yu840915 <yu840915@gmail.com>
2026-07-30 16:06:04 +0800

    Tweak buffer info

--- Sources/RemoteCameraCore/DataTypes/VideoBufferInfo.swift
+++ Sources/RemoteCameraCore/DataTypes/VideoBufferInfo.swift
@@ -1,18 +1,15 @@
 public struct VideoBufferInfo: Sendable, Equatable {
   public var dimensions: ImageDimensions
   public var orientation: ImageOrientation?
-  public let inputDeviceDirection: DeviceDirection?
   public var mirroring: VideoMirroring
 
   public init(
     dimensions: ImageDimensions,
     orientation: ImageOrientation?,
-    inputDeviceDirection: DeviceDirection? = nil,
     mirroring: VideoMirroring = .none
   ) {
     self.dimensions = dimensions
     self.orientation = orientation
-    self.inputDeviceDirection = inputDeviceDirection
     self.mirroring = mirroring
   }
 }
