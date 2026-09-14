5c81aeb49d04dd865f00d5c9156db4f5aa64a754
yu840915 <yu840915@gmail.com>
2026-09-14 22:21:58 +0800

    Add camera details

--- Sources/RemoteCameraCore/Camera/CameraDescriptor.swift
+++ Sources/RemoteCameraCore/Camera/CameraDescriptor.swift
@@ -2,16 +2,27 @@ public struct CameraDescriptor: Sendable, Equatable {
   public let id: String
   public let name: String
   public let position: CameraPosition
+  public let focalLength: Double?
+  public let aperture: Double?
 
-  public init(id: String, name: String, position: CameraPosition) {
+  public init(
+    id: String,
+    name: String,
+    position: CameraPosition,
+    focalLength: Double? = nil,
+    aperture: Double? = nil
+  ) {
     self.id = id
     self.name = name
     self.position = position
+    self.focalLength = focalLength
+    self.aperture = aperture
   }
 }
 
 extension CameraDescriptor: CustomStringConvertible {
   public var description: String {
-    return "Camera(id: \(id), name: \(name), position: \(position))"
+    return
+      "Camera(id: \(id), name: \(name), position: \(position), focalLength: \(focalLength ?? 0.0), aperture: \(aperture ?? 0.0))"
   }
 }
